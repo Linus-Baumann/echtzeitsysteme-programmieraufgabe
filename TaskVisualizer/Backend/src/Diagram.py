b8c749b5393032c1b39d2ee875e935f6dd5abad1
Skeletron5 <inf20140@lehre.dhbw-stuttgart.de>
2023-03-11 08:27:11 +0100

    first steps with Classes

--- TaskVisualizer/Backend/src/Diagram.py
+++ TaskVisualizer/Backend/src/Diagram.py
@@ -4,13 +4,17 @@ from typing import List
 from Abstracts import IDiagram
 
 class Diagram(IDiagram):
+# Müssen wir hier nicht die Variablen (Arrays) erstellen, nicht im Constructor da wir sie 
+# dort doch noch gar nicht kennen???
+    
     def __init__(self):
         self._tasks = np.array
         self._activities = np.array
         self._semaphores = np.array
         self._mutexes = np.array
 
-    def parse(seld, rows):
+# rows from FileReader
+    def parse(self, rows):
         for object in rows:
             if object[0] == "Task":
                 print("lol")
