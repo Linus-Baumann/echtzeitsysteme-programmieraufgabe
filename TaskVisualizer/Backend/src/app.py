93423c067e4a166b2ea6bce9afe45f460069750e
Linus Baumann <inf20001@lehre.dhbw-stuttgart.de>
2023-03-23 19:00:59 +0100

    api moved to Backend folder

---
+++ TaskVisualizer/Backend/src/app.py
@@ -0,0 +1,27 @@
+import json
+from flask import Flask, render_template, request, send_file
+from Diagram import Diagram
+from FileReader import FileReader
+import os
+
+app = Flask(__name__)
+
+file_reader = FileReader()
+diagram = Diagram()
+
+# Pfad zur CSV-Datei mit der Konfiguration
+filepath = "./static/csv/example-structure.csv"
+
+rows = file_reader.open(filepath)
+
+diagram.generate(rows)
+
+@app.route('/visualizer-api/diagram', methods=['GET'])
+def get_diagram():
+    diagram.execute_cycle()
+    diagram.draw_graph()
+    file_path = "./static/images/testGraph.png"
+    return send_file(file_path, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
