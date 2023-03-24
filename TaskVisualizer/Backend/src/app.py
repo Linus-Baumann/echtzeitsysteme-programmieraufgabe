14eea55b4e65099f58952fc7cb48c790d0f9f146
Linus Baumann <inf20001@lehre.dhbw-stuttgart.de>
2023-03-24 18:12:10 +0100

    reset endpoint from simon added

--- TaskVisualizer/Backend/src/app.py
+++ TaskVisualizer/Backend/src/app.py
@@ -1,28 +1,41 @@
-import json
-from flask import Flask, render_template, request, send_file
+import json, os, time
+from flask import Flask, jsonify, render_template, request, send_file
 from Diagram import Diagram
 from FileReader import FileReader
-import os
 
 app = Flask(__name__)
 
 file_reader = FileReader()
 diagram = Diagram()
 
-# Pfad zur CSV-Datei mit der Konfiguration
-filepath = "./TaskVisualizer/Backend/src/static/csv/example-structure.csv"
+startup_executed = False
 
-rows = file_reader.open(filepath)
+if not startup_executed:
+    # Pfad zur CSV-Datei mit der Konfiguration
+    source_filepath = "./static/csv/example-structure.csv"
 
-diagram.generate(rows)
+    rows = file_reader.open(source_filepath)
+
+    diagram.generate(rows)
+    startup_executed = True
+
+@app.route('/visualizer-api/reset-diagram', methods=['GET'])
+def origin_status():
+    diagram.reset()
+    diagram.generate(rows)
+    diagram.draw_graph()
+    time.sleep(2)
+    return jsonify("Success")
 
 @app.route('/visualizer-api/diagram', methods=['GET'])
 def get_diagram():
     diagram.execute_cycle()
     diagram.draw_graph()
     print(os.getcwd())
-    file_path = "./TaskVisualizer/Backend/src/static/images/testGraph.png"
+    file_path = "./static/images/testGraph.png"
     return send_file(file_path, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
